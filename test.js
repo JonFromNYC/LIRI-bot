5ebcf0ce247274d8bf3c7676d891b13fb8882fca
JonFromNYC <82.Garcia@gmail.com>
2018-12-08 23:29:04 -0500

    axios and spotify calls work

--- test.js
+++ test.js
@@ -1,31 +1,44 @@
+// Required
 require("dotenv").config();
-
 var Spotify = require('node-spotify-api');
-
 var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
+// Variables to hold user input
 var command = process.argv[2]; // HOLD THE COMMAND
 var inputString = process.argv.slice(3).join(" "); // USER INPUT AFTER COMMAND
 
-var bandsAPI = "https://rest.bandsintown.com/artists/" + 
-                artist + 
+var bandsInTownAPI = "https://rest.bandsintown.com/artists/" + 
+                inputString +
                 "/events?app_id=codingbootcamp";
 
 var spotify = new Spotify(keys.spotify);
 
+if (command === "concert-this") {
+    console.log("\nChecking Bands in Town\n");
+    getConcertInfo();
+} else if (command === "spotify-this-song") {
+    console.log("\nCalling Spotify\n");
+    getSpotifyInfo();
+}
+
+// --- Functions
+function getSpotifyInfo() {
     spotify.search({
         type: 'track',
-        query: 'All the Small Things'
+        query: inputString
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
         console.log(data);
+        // data.tracks.items[args]
     });
+}
+
 
 // Functions for each command type
 // concert-this
@@ -33,4 +46,12 @@ var spotify = new Spotify(keys.spotify);
 // movie-this
 // do-what-it-says
 
-
+function getConcertInfo() {
+    axios.get(bandsInTownAPI)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
